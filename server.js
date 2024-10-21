2d11f98c87fbf1519843cc8fb729be015ec60014
unknown <ezequieloliveiraes@outlook.com>
2024-10-20 22:27:48 -0300

    tratando erros

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', dadosRoutes )
 
-// Inicia o servidor
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 })
--- src/controllers/dadosController.js
+++ src/controllers/dadosController.js
@@ -2,32 +2,56 @@ const axios = require("axios")
 
 const getProducts = async (req, res) => {
     const { termoPesquisado, precoMinimo, precoMaximo, condicao } = req.query
-    console.log('Condicao:', condicao);
+
+    if (!termoPesquisado) {
+        return res.status(400).send('Termo pesquisado é obrigatório.')
+    }
+
+    if (!precoMinimo) {
+        return res.status(400).send('Preço mínimo não preenchido.')
+    }
+
+    if (!precoMaximo) {
+        return res.status(400).send('Preço máximo não preenchido.')
+    }
+
+    // Valida se os preços são válidos
+    const minPrice = Number(precoMinimo)
+    const maxPrice = Number(precoMaximo)
+
+    if (isNaN(minPrice)) {
+        return res.status(400).send('O preço mínimo deve ser um número válido.')
+    }
+
+    if (isNaN(maxPrice)) {
+        return res.status(400).send('O preço máximo deve ser um número válido.')
+    }
+
     try {
         const response = await axios.get('https://api.mercadolibre.com/sites/MLB/search', {
-            params: { q: termoPesquisado, price: `${precoMinimo}-${precoMaximo}`, condition: `${condicao}` }
-            
+            params: {
+                q: termoPesquisado,
+                price: `${minPrice}-${maxPrice}`,
+                condition: condicao,
+            },
         })
-        
-        // if (condicao) {
-        //     params.condition = condicao;
-        // }
-        console.log(response.data)
+        if(response.data.results <= 0) {
+            return res.status(400).send('Nenhum produto encontrado com esses critérios.')
+        }
 
-        res.json(response.data)
+        const products = response.data.results
+        res.json({ results: products })
 
     } catch (error) {
-
-        console.error(error)
+        console.error('Erro ao buscar dados da API:', error)
         res.status(500).send("Erro ao buscar dados da API")
     }
 }
 
 module.exports = {
-    getProducts
+    getProducts,
 }
 
-
-// Documentacao que explica essa url
-//https://developers.mercadolivre.com.br/pt_br/categorias-e-atributos-veiculos
-//https://developers.mercadolivre.com.br/pt_br/itens-e-buscas
+// Documentação que explica essa URL
+// https://developers.mercadolivre.com.br/pt_br/categorias-e-atributos-veiculos
+// https://developers.mercadolivre.com.br/pt_br/itens-e-buscas
